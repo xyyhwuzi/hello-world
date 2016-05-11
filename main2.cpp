799ea223cdb58c284bec42c6674c22a77e9c8163
Wuzi <wuzi1985@163.com>
2016-05-11 14:57:19 +0800

    Update main2.cpp
    Update main2.cpp, add time description

--- main2.cpp
+++ main2.cpp
@@ -1,4 +1,5 @@
 //main2
+// ccc 2016-05-11 14:56
 
 #include <iostream>
 
@@ -6,6 +7,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	cout<<"¼ÓÓÍ£¬ÏÄæ÷"<<endl;
+	cout<<"Â¼Ã“Ã“ÃÂ£Â¬ÃÃ„Ã¦Ã·"<<endl;
 	return 0;
 }
